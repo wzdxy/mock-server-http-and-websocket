6360ac91caa78cf6ac68b484c28a8f9597528538
zc <zc_93@qq.com>
2017-12-07 21:50:14 +0800

    增加注释 改用 ejs 模版引擎

--- app.js
+++ app.js
@@ -1,25 +1,37 @@
-/**
- * Created by zc on 2017/8/1.
- */
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var cppRouters = require('./routers/cppServer');
-var nodeRouters = require('./routers/nodeServer');
-var webSocket = require('./routers/webSocket');
-var colors = require('colors');
-let log=require("./utils/log");
-let config=require("./config")
+const express = require('express');
+const bodyParser = require('body-parser');
+const colors = require('colors');
+const ejs = require('ejs');
+const open = require("open");
 
+const cppRouters = require('./routers/cppServer');
+const nodeRouters = require('./routers/nodeServer');
+const webSocket = require('./routers/webSocket');
+const log=require("./utils/log");
+const config=require("./config")
+
+const app = express();
+// 设置模板引擎 ejs
+app.set('view engine','ejs') ;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// 挂载静态资源
+app.use('/assets', express.static('assets'))
+
+// 跨域支持
 app.all('*',function(req,res,next){
     res.header('Access-Control-Allow-Origin','*')
     next();
 })
-app.use('/page', express.static('page'))
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 
+// 控制台页面
+app.get('/', function (req, res) {        
+    res.render('control',{config:config})
+});
 
+// 接收控制台命令
 app.post('/ws',function(req,res){
     let sentContent=req.body.body;
     let count=0;
@@ -33,26 +45,25 @@ app.post('/ws',function(req,res){
     res.send({code:0,data:sentContent,count:count});
 });
 
-app.get('/', function (req, res) {
-    res.send('Hello Mock!');
-});
-app.use(cppRouters);
+// node 服务器的 mock
 app.use(nodeRouters);
-
-
-var nodeServer = app.listen(config.nodePort, function () {
-    var host = nodeServer.address().address;
+const nodeServer = app.listen(config.nodePort, function () {
+    let host = nodeServer.address().address;
     console.log('Node 代理 Mock 已启动 , listening at http://%s:%s'.white.bgGreen, host, config.nodePort);    
 });
 
-var cppServer = app.listen(config.cppPort, function () {
-    var host = cppServer.address().address;
+// cpp 服务器的 mock
+app.use(cppRouters);
+const cppServer = app.listen(config.cppPort, function () {
+    let host = cppServer.address().address;
     console.log('Cpp 代理 Mock 已启动 , listening at http://%s:%s'.white.bgGreen, host, config.cppPort);
 });
 
-var wsArray=webSocket.start(config.wsPort);
+// 启动 websocket 服务器
+const wsArray=webSocket.start(config.wsPort);
 
-var controlPanelServer=app.listen(config.controlPagePort)
+// 控制台网页服务
+const controlPanelServer=app.listen(config.controlPagePort)
 
-var open = require("open");
-open("http://127.0.0.1:"+config.controlPagePort+"/page/control.html");
+// 打开控制台网页窗口
+open("http://127.0.0.1:"+config.controlPagePort);
--- utils/log.js
+++ utils/log.js
@@ -4,7 +4,11 @@ function logToConsole(req, resBody) {
     let methodStr=req.method;
     let urlStr=req.originalUrl;
     let ipStr=req.ip;
+    
+    // 打印请求信息
     console.log(methodStr.bgCyan.white.bold + " " + urlStr.magenta + " from " + ipStr.cyan + " at "+timeStr.gray + "\nrequest body >>>\n".yellow, JSON.stringify(req.body, null, 4).white);
+    
+    // 打印返回值信息
     console.log("response body >>> \n".green, JSON.stringify(resBody, null, 4).white+"\n");
 }
 
