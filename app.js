6bc884128cb54ed351fb2edcca3038d5169a206f
zc <zc_93@qq.com>
2017-12-07 11:30:42 +0800

    提取 ws

--- app.js
+++ app.js
@@ -3,64 +3,15 @@
  */
 var express = require('express');
 var app = express();
-var WebSocket=require('ws')
 var bodyParser = require('body-parser');
 var cppRouters = require('./routers/cppServer');
 var nodeRouters = require('./routers/nodeServer');
+var webSocket = require('./routers/webSocket');
 var colors = require('colors');
 let log=require("./utils/log");
 
-app.use(bodyParser.json()); // for parsing application/json
 
-var wsArray=[];
-var wsPort = 9001
-var socket=new WebSocket.Server({
-    port:wsPort
-},function(){
-    var host = nodeServer.address().address;
-    var port = nodeServer.address().port;
-    console.log('webSocket Mock 已启动 , listenning as ws://:::%s'.white.bgGreen, wsPort);
-})
-socket.on('connection',function(ws){
-    wsArray.push(ws);
-    console.log('WebSocket Connected'.bgCyan+' at '+new Date().toLocaleString().gray+' Total Client(s):'+wsArray.filter(ws=>ws.OPEN===ws.readyState).length+"\n");
-    let status=2,active=2,database=2,net=2;
-    setInterval(function () {    
-        let str=JSON.stringify({
-            Type:"ServerStatus",
-            Status:status,
-            Server:"192.168.3.48",
-            Params:{
-                Active:active,
-                Database:database,   
-                Net:net
-            }
-        })
-        // console.log(str);        
-        if(ws.readyState===1){
-            ws.send(str);
-        }
-        status=status===2?5:2;
-        active=status===2?2:5;
-        database=status===2?2:5;                
-    },2000)
-    ws.onopen=function(){
-        console.log('open');                
-    }
-    ws.onmessage = function(){
-        
-    }
-    ws.onclose = function(e,a){
-        console.log('WebSocket Closed'.bgYellow+' Code:'+String(e.code).green+' '+e.reason+' at '+new Date().toLocaleString().gray+' Total Client(s):'+wsArray.filter(ws=>ws.OPEN===ws.readyState).length+"\n");    
-    }
-    ws.onerror = function(e){
-        console.log('error',e)
-    }
-})
-
-app.use(function(req,res,next){
-    next();
-});  
+var wsArray=webSocket.start(9001);
 app.post('/ws',function(req,res){
     let sentContent=req.body;
     let count=0;
@@ -75,7 +26,7 @@ app.post('/ws',function(req,res){
 });
 
 app.get('/', function (req, res) {
-    res.send('Hello Mock!');
+    res.send('Hello Mock!');    
 });
 app.use(cppRouters);
 app.use(nodeRouters);
@@ -84,14 +35,12 @@ app.use(nodeRouters);
 var nodeServer = app.listen(8122, function () {
     var host = nodeServer.address().address;
     var port = nodeServer.address().port;
-
     console.log('Node 代理 Mock 已启动 , listening at http://%s:%s'.white.bgGreen, host, port);    
 });
 
 var cppServer = app.listen(8126, function () {
     var host = cppServer.address().address;
     var port = cppServer.address().port;
-
     console.log('Cpp 代理 Mock 已启动 , listening at http://%s:%s'.white.bgGreen, host, port);
 });
 
--- routers/webSocket.js
+++ routers/webSocket.js
@@ -1,1 +1,47 @@
-var WebSocket=require('ws')
+function start(wsPort){
+    var WebSocket=require('ws')
+    var wsArray=[];
+    var socket=new WebSocket.Server({
+        port:wsPort
+    },function(){
+        console.log('webSocket Mock 已启动 , listenning as ws://:::%s'.white.bgGreen, wsPort);
+    })
+    socket.on('connection',function(ws){
+        wsArray.push(ws);
+        console.log('WebSocket Connected'.bgCyan+' at '+new Date().toLocaleString().gray+' Total Client(s):'+wsArray.filter(ws=>ws.OPEN===ws.readyState).length+"\n");
+        let status=2,active=2,database=2,net=2;
+        setInterval(function () {    
+            let str=JSON.stringify({
+                Type:"ServerStatus",
+                Status:status,
+                Server:"192.168.3.48",
+                Params:{
+                    Active:active,
+                    Database:database,   
+                    Net:net
+                }
+            })
+            // console.log(str);        
+            if(ws.readyState===1){
+                ws.send(str);
+            }
+            status=status===2?5:2;
+            active=status===2?2:5;
+            database=status===2?2:5;                
+        },2000)
+        ws.onopen=function(){
+            console.log('open');                
+        }
+        ws.onmessage = function(){
+            
+        }
+        ws.onclose = function(e,a){
+            console.log('WebSocket Closed'.bgYellow+' Code:'+String(e.code).green+' '+e.reason+' at '+new Date().toLocaleString().gray+' Total Client(s):'+wsArray.filter(ws=>ws.OPEN===ws.readyState).length+"\n");    
+        }
+        ws.onerror = function(e){
+            console.log('error',e)
+        }
+    })
+    return wsArray;
+}
+module.exports={start:start};
